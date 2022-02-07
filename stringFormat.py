2a2198bcf5ac59f7cea2f38232dec0e56f68455f
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-07 16:19:45 -0500

    Done

---
+++ stringFormat.py
@@ -0,0 +1,9 @@
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print("My name is {} {} and I am {} years old.".format(first_name, last_name, age))
+# output: My name is Zen Coder and I am 27 years old.
+print("My name is {} {} and I am {} years old.".format(age, first_name, last_name))
+# output: My name is 27 Zen and I am Coder years old.
+
+print("My age is {} and my name is {} {}.".format(age, first_name, last_name))
