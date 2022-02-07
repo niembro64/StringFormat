3fa4ae86c7afe3a625395bfd7ceac9119ab66afc
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-07 16:26:39 -0500

    Added Other

--- stringFormat.py
+++ stringFormat.py
@@ -7,3 +7,14 @@ print("My name is {} {} and I am {} years old.".format(age, first_name, last_nam
 # output: My name is 27 Zen and I am Coder years old.
 
 print("My age is {} and my name is {} {}.".format(age, first_name, last_name))
+
+
+hw = "Hello %s" % "World"
+py = "I love Python %d" % 3
+print(hw, py)
+
+name = "Zen"
+age = 27
+print("My name is %s and I'm %d" % (name, age))		# or with variables
+# print("My name is %s and I'm %d" % (age, name))		# or with variables
+# output: My name is Zen and I'm 27
